170b33e219d370e5df310157bb4d88abd4bf0502
JPisaBrony <jdpohl789@gmail.com>
2015-02-05 23:55:33 -0600

    ^ project, main, test.blend fixed some project settings removed a test file

--- Dungeon-Shambles/Dungeon-Shambles/Main.cs
+++ Dungeon-Shambles/Dungeon-Shambles/Main.cs
@@ -4,17 +4,18 @@ using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Input;
-using aimp = Assimp;
 
 namespace DungeonShambles
 {
 	class Game: GameWindow
 	{
+		static ModelImporter m = new ModelImporter ();
+
 		public static void Main (string[] args)
 		{
 			using (Game game = new Game())
 			{
-				modelImporter ("test.blend");
+				m.importModel ("cube.blend");
 				init ();
 				game.Run(30);
 			}
@@ -24,7 +25,7 @@ namespace DungeonShambles
 			GL.ClearColor (0.0f, 0.0f, 0.0f, 0.0f);
 			GL.MatrixMode (MatrixMode.Projection);
 			GL.LoadIdentity ();
-			GL.Ortho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+			GL.Ortho(10.0, 10.0, 10.0, 10.0, -10.0, 10.0);
 		}
 
 		protected override void OnLoad(EventArgs e)
@@ -37,21 +38,18 @@ namespace DungeonShambles
 		protected override void OnRenderFrame(FrameEventArgs e)
 		{
 			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-			GL.Color3(Color.FromArgb(239, 80, 145));
-
+			//GL.Color3(Color.FromArgb(239, 80, 145));
+			/*
 			GL.Begin(PrimitiveType.Quads);
 			GL.Vertex2 (0.25, 0.75);
 			GL.Vertex2 (0.75, 0.75);
 			GL.Vertex2 (0.75, 0.25);
 			GL.Vertex2 (0.25, 0.25);
 			GL.End ();
+			*/
+			m.renderModel ();
 
 			SwapBuffers();
-		}
-
-		public static void modelImporter(String name)
-		{
-			aimp.AssimpContext importer = new aimp.AssimpContext();
 		}
 
 		protected override void OnResize(EventArgs e) {}
