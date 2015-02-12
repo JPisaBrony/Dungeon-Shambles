25b9ac4a00d50e14b307ea113bc9c2efe19a17bc
joshuaphilippy <joshuaphilippy@gmail.com>
2015-02-12 15:14:50 -0600

    +other stuff

---
+++ Dungeon-Shambles/Dungeon-Shambles/Items/Weapon/MagicWeapon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DungeonShambles.Cranting;
+
+namespace DungeonShambles.Items
+{
+    class MagicWeapon : Weapon
+    {
+        public MagicWeapon()
+            : base(1, 1, 1, 3)
+        {
+            
+        }
+    }
+}
---
+++ Dungeon-Shambles/Dungeon-Shambles/Items/Weapon/PhysicalWeapon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonShambles.Items
+{
+    class PhysicalWeapon : Weapon
+    {
+        public PhysicalWeapon()
+            : base(1, 1, 3, 1)
+        {
+
+        }
+    }
+}
