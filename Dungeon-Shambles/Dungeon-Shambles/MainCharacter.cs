5a265ceba8ced04f9de259695d66acc154322b5c
JPisaBrony <jdpohl789@gmail.com>
2015-02-14 22:37:34 -0600

    + MainCharacter added a main character

--- Dungeon-Shambles/Dungeon-Shambles/MainCharacter.cs
+++ Dungeon-Shambles/Dungeon-Shambles/MainCharacter.cs
@@ -4,9 +4,30 @@ namespace DungeonShambles
 {
 	public class MainCharacter
 	{
+		// create a texture for the character
+		TextureImporter character = new TextureImporter ();
+		// character x and y position
+		float x, y;
+
 		public MainCharacter ()
 		{
+			character.importTexture ("twi.jpg");
+			x = 0f;
+			y = 0f;
 		}
+
+		public void renderCharacter() {
+			character.renderTexture (0.5f, x, y);
+		}
+
+		public void increaseX(float theX) {
+			x += theX;
+		}
+
+		public void increaseY(float theY) {
+			y += theY;
+		}
+
 	}
 }
 
