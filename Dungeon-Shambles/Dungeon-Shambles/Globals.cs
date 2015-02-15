1cf8dc1e82e6ef07197c6983d9a71d23ba688afa
JPisaBrony <jdpohl789@gmail.com>
2015-02-15 12:36:20 -0600

    ^ globals

--- Dungeon-Shambles/Dungeon-Shambles/Globals.cs
+++ Dungeon-Shambles/Dungeon-Shambles/Globals.cs
@@ -5,9 +5,8 @@ namespace DungeonShambles
 	public abstract class Globals
 	{
 		// the windows width and height
-		public static int WindowWidth = 640, WindowHeight = 480;
-
-
+		public static int WindowWidth = 800, WindowHeight = 640;
+		public static float TextureSize = 0.1f;
 	}
 }
 
