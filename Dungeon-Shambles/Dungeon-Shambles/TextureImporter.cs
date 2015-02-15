de382e89afabcc2c1f565cb70cb4a2c34258f750
JPisaBrony <jdpohl789@gmail.com>
2015-02-14 22:38:06 -0600

    + TextureImporter created a class for importing textures

--- Dungeon-Shambles/Dungeon-Shambles/TextureImporter.cs
+++ Dungeon-Shambles/Dungeon-Shambles/TextureImporter.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace DungeonShambles
 {
 	public class TextureImporter
 	{
-		public TextureImporter ()
-		{
+		// a reference to the texture to pass between the methods
+		private int texture;
+
+		public TextureImporter () {}
+
+		public void importTexture(String name) {
+			Bitmap tex = new Bitmap (name);
+
+			GL.GenTextures(1, out texture);
+			GL.BindTexture(TextureTarget.Texture2D, texture);
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+
+			BitmapData data = tex.LockBits(new System.Drawing.Rectangle(0, 0, tex.Width, tex.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+			GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+
+			tex.UnlockBits(data);
 		}
+
+		public void removeTexture() {
+			GL.DeleteTextures(1, ref texture);
+		}
+
+		public void renderTexture(float size, float x, float y) {
+			GL.BindTexture(TextureTarget.Texture2D, texture);
+
+			GL.Begin(PrimitiveType.Quads);
+
+			GL.TexCoord2(0.0f, 1.0f); GL.Vertex2(x + size * -1, y + size * -1);
+			GL.TexCoord2(1.0f, 1.0f); GL.Vertex2(x + size, y + size * -1);
+			GL.TexCoord2(1.0f, 0.0f); GL.Vertex2(x + size, y + size);
+			GL.TexCoord2(0.0f, 0.0f); GL.Vertex2(x + size * -1, y + size);
+
+			GL.End();
+		}
+
 	}
 }
 
