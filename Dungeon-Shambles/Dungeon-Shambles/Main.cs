6c39ca69125319688e8da57b7c47b6bf96cfd7bf
JPisaBrony <jdpohl789@gmail.com>
2015-02-14 22:34:29 -0600

    ^ Main.cs + Keyboard Input made some base changes and fixed some OpenGL calls gave some basic input for the main character

--- Dungeon-Shambles/Dungeon-Shambles/Main.cs
+++ Dungeon-Shambles/Dungeon-Shambles/Main.cs
@@ -9,46 +9,78 @@ namespace DungeonShambles
 {
 	class Game: GameWindow
 	{
-		static ModelImporter m = new ModelImporter ();
+		// object reference to pass between OnLoad and OnRenderFrame
+		MainCharacter mainChar;
+
+		// setup the window width and height
+		public Game() : base(Globals.WindowWidth, Globals.WindowHeight) {}
 
 		public static void Main (string[] args)
 		{
 			using (Game game = new Game())
 			{
-				m.importModel ("cube.blend");
-				init ();
+				// run the game at 30 frames per second
 				game.Run(30);
 			}
 		}
 
-		private static void init() {
+		private void init() {
+			// clear the window to white
 			GL.ClearColor (0.0f, 0.0f, 0.0f, 0.0f);
-			GL.MatrixMode (MatrixMode.Projection);
+
+			GL.MatrixMode (MatrixMode.Modelview);
+
 			GL.LoadIdentity ();
-			GL.Ortho(10.0, 10.0, 10.0, 10.0, -10.0, 10.0);
+			// setup the view port
+			GL.Ortho(1.0, 1.0, 1.0, 1.0, 0.0, 4.0);
+			// enable textures to be rendered
+			GL.Enable(EnableCap.Texture2D);
 		}
 
 		protected override void OnLoad(EventArgs e)
 		{
 			base.OnLoad(e);
+			// base setup
+			init ();
+			// set the windows title
 			Title = "Dungeon Shambles";
+			// clear the color of the window to twilight sparkle's magenta
 			GL.ClearColor(Color.FromArgb (204, 159, 213));
+			// create the main character
+			mainChar = new MainCharacter ();
+		}
+
+		protected override void OnUpdateFrame(FrameEventArgs e)
+		{
+			// variable for checking keyboard input
+			var keyboard = OpenTK.Input.Keyboard.GetState();
+			// left key is pressed
+			if (keyboard [OpenTK.Input.Key.A])
+				// increase the main characters x position
+				mainChar.increaseX (-0.01f);
+			// right key is pressed
+			else if (keyboard [OpenTK.Input.Key.D])
+				// decrease the main characters x position
+				mainChar.increaseX (0.01f);
+			// up key is pressed
+			if (keyboard [OpenTK.Input.Key.W])
+				// increase the main characters y position
+				mainChar.increaseY (0.01f);
+			// down key is pressed
+			else if (keyboard [OpenTK.Input.Key.S])
+				// decrease the main characters x position
+				mainChar.increaseY (-0.01f);
 		}
 
 		protected override void OnRenderFrame(FrameEventArgs e)
 		{
+			// clear the screen
 			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-			//GL.Color3(Color.FromArgb(239, 80, 145));
-			/*
-			GL.Begin(PrimitiveType.Quads);
-			GL.Vertex2 (0.25, 0.75);
-			GL.Vertex2 (0.75, 0.75);
-			GL.Vertex2 (0.75, 0.25);
-			GL.Vertex2 (0.25, 0.25);
-			GL.End ();
-			*/
-			m.renderModel ();
 
+			// render the main character
+			mainChar.renderCharacter ();
+
+			// switch between the 2 buffers
 			SwapBuffers();
 		}
 
