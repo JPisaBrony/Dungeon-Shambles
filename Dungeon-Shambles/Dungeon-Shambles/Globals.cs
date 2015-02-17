0a8d0cbe3201a29d057c6743e5632c5917559de8
JPisaBrony <jdpohl789@gmail.com>
2015-02-16 22:15:30 -0600

    ^ Globals added a comment

--- Dungeon-Shambles/Dungeon-Shambles/Globals.cs
+++ Dungeon-Shambles/Dungeon-Shambles/Globals.cs
@@ -6,6 +6,7 @@ namespace DungeonShambles
 	{
 		// the windows width and height
 		public static int WindowWidth = 800, WindowHeight = 640;
+		// size of all textures
 		public static float TextureSize = 0.1f;
 	}
 }
