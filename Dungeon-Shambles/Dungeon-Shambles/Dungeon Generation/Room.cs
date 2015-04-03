3bd3ff86f13cddfb6139a0d7f9b8bbb02561778a
JPisaBrony <jdpohl789@gmail.com>
2015-04-03 17:11:35 -0500

    ^ room generation fixed room rendering and made each room have edge tiles

--- Dungeon-Shambles/Dungeon-Shambles/Dungeon Generation/Room.cs
+++ Dungeon-Shambles/Dungeon-Shambles/Dungeon Generation/Room.cs
@@ -4,37 +4,42 @@ namespace DungeonShambles
 {
 	public class Room
 	{
-		private Tile[,] tiles = new Tile[10,10];
+		private Tile[,] tiles;
 		private int roomWidth, roomHeight;
 		private string tileName;
+		private string wallName;
 		private int tileSize;
 
-		public Room (string tile, int tSize) {
+		public Room (string tile, string wall, int tSize) {
 			tileName = tile;
 			tileSize = tSize;
+			wallName = wall;
 		}
 
 		public void generateRoom(int w, int h) {
+			tiles = new Tile[w, h];
 			roomWidth = w;
 			roomHeight = h;
 			for (int i = 0; i < w; i++) {
 				for (int j = 0; j < h; j++) {
-					tiles[i,j] = new Tile (tileName, tileSize, tileSize);
+					if(i == 0 || j == 0 || i == w || j == h)
+						tiles[i,j] = new Tile (wallName, tileSize, tileSize, true);
+					tiles[i,j] = new Tile (tileName, tileSize, tileSize, false);
 				}
 			}
-
 		}
 
 		public void renderRoom() {
-
 			for (int i = 0; i < roomWidth; i++) {
 				for (int j = 0; j < roomHeight; j++) {
-					tiles [i,j].renderTile (Globals.TextureSize, i * Globals.TextureSize, j * Globals.TextureSize);
+					tiles [i,j].renderTile (Globals.TextureSize, i * Globals.TextureSize * 2, j * Globals.TextureSize * 2);
 				}
 			}
-
 		}
 
+		public Tile getTileAtLocation(int x, int y) {
+			return tiles [x, y];
+		}
 	}
 }
 
