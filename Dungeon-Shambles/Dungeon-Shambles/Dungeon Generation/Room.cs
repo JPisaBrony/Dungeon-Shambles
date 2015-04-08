42efea1ee3691625f01bdabda6c29b0c6de2ee7f
JPisaBrony <jdpohl789@gmail.com>
2015-04-08 01:00:16 -0500

    ^ room generation made the room look more like a room with walls and openings for doors

--- Dungeon-Shambles/Dungeon-Shambles/Dungeon Generation/Room.cs
+++ Dungeon-Shambles/Dungeon-Shambles/Dungeon Generation/Room.cs
@@ -6,39 +6,78 @@ namespace DungeonShambles
 	{
 		private Tile[,] tiles;
 		private int roomWidth, roomHeight;
-		private string tileName;
-		private string wallName;
+		private string[] tileNames;
 		private int tileSize;
+		private int numberOfDoors;
 
-		public Room (string tile, string wall, int tSize) {
-			tileName = tile;
+		public Room (string[] tNames, int tSize) {
 			tileSize = tSize;
-			wallName = wall;
+			tileNames = tNames;
 		}
 
-		public void generateRoom(int w, int h) {
+		public void generateRoom(int w, int h, int doors) {
 			tiles = new Tile[w, h];
 			roomWidth = w;
 			roomHeight = h;
+			numberOfDoors = doors;
+			int doorsPlaced = 0;
 			for (int i = 0; i < w; i++) {
 				for (int j = 0; j < h; j++) {
-					if(i == 0 || j == 0 || i == w || j == h)
-						tiles[i,j] = new Tile (wallName, tileSize, tileSize, true);
-					tiles[i,j] = new Tile (tileName, tileSize, tileSize, false);
+					if (i == 0 && j == 0) {
+						tiles [i, j] = new Tile (tileNames [5], tileSize, tileSize, true);
+					} else if (i == 0 && j == h - 1) {
+						tiles [i, j] = new Tile (tileNames [6], tileSize, tileSize, true);
+					} else if (i == w - 1 && j == h - 1) {
+						tiles [i, j] = new Tile (tileNames [7], tileSize, tileSize, true);
+					} else if (i == w - 1 && j == 0) {
+						tiles [i, j] = new Tile (tileNames [8], tileSize, tileSize, true);
+					} else if (i == 0) {
+						tiles [i, j] = new Tile (tileNames [1], tileSize, tileSize, true);
+						placeDoor (ref doorsPlaced, i, j);
+					} else if (j == h - 1) {
+						tiles [i, j] = new Tile (tileNames [2], tileSize, tileSize, true);
+						placeDoor (ref doorsPlaced, i, j);
+					} else if (i == w - 1) {
+						tiles [i, j] = new Tile (tileNames [3], tileSize, tileSize, true);
+						placeDoor (ref doorsPlaced, i, j);
+					} else if (j == 0) {
+						tiles [i, j] = new Tile (tileNames [4], tileSize, tileSize, true);
+						placeDoor (ref doorsPlaced, i, j);
+					} else {
+						tiles [i, j] = new Tile (tileNames [0], tileSize, tileSize, false);
+					}
 				}
 			}
 		}
 
-		public void renderRoom() {
+		private void placeDoor(ref int doorsPlaced, int i, int j) {
+			Random rng = new Random ();
+			if(rng.Next(0, 5) == 0) {
+				if (doorsPlaced < numberOfDoors) {
+					tiles [i, j] = new Tile (tileNames [0], tileSize, tileSize, false);
+					doorsPlaced++;
+				}
+			}
+		}
+
+		public void renderRoom(int offsetX, int offsetY) {
 			for (int i = 0; i < roomWidth; i++) {
 				for (int j = 0; j < roomHeight; j++) {
-					tiles [i,j].renderTile (Globals.TextureSize, i * Globals.TextureSize * 2, j * Globals.TextureSize * 2);
+					tiles [i,j].renderTile (Globals.TextureSize, i * Globals.TextureSize * 2 + offsetX, j * Globals.TextureSize * 2 + offsetY);
 				}
 			}
 		}
 
 		public Tile getTileAtLocation(int x, int y) {
 			return tiles [x, y];
+		}
+
+		public int getRoomWidth() {
+			return roomWidth;
+		}
+
+		public int getRoomHeight() {
+			return roomHeight;
 		}
 	}
 }
