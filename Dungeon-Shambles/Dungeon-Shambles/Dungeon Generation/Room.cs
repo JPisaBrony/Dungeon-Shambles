4017fba610502374917294838cd6a8cba088dd98
joshuaphilippy <joshuaphilippy@gmail.com>
2015-04-27 09:45:38 -0500

    merging yo

--- Dungeon-Shambles/Dungeon-Shambles/Dungeon Generation/Room.cs
+++ Dungeon-Shambles/Dungeon-Shambles/Dungeon Generation/Room.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 
 namespace DungeonShambles
@@ -46,3 +47,89 @@ namespace DungeonShambles
 	}
 }
 
+=======
+﻿using System;
+
+namespace DungeonShambles
+{
+	public class Room
+	{
+		private Tile[,] tiles;
+		private int roomWidth, roomHeight;
+		private string[] tileNames;
+		private int tileSize;
+		private int numberOfDoors;
+
+		public Room (string[] tNames, int tSize) {
+			tileSize = tSize;
+			tileNames = tNames;
+		}
+
+		public void generateRoom(int w, int h, int doors) {
+			tiles = new Tile[w, h];
+			roomWidth = w;
+			roomHeight = h;
+			numberOfDoors = doors;
+			int doorsPlaced = 0;
+			for (int i = 0; i < w; i++) {
+				for (int j = 0; j < h; j++) {
+					if (i == 0 && j == 0) {
+						tiles [i, j] = new Tile (tileNames [5], tileSize, tileSize, true);
+					} else if (i == 0 && j == h - 1) {
+						tiles [i, j] = new Tile (tileNames [6], tileSize, tileSize, true);
+					} else if (i == w - 1 && j == h - 1) {
+						tiles [i, j] = new Tile (tileNames [7], tileSize, tileSize, true);
+					} else if (i == w - 1 && j == 0) {
+						tiles [i, j] = new Tile (tileNames [8], tileSize, tileSize, true);
+					} else if (i == 0) {
+						tiles [i, j] = new Tile (tileNames [1], tileSize, tileSize, true);
+						placeDoor (ref doorsPlaced, i, j);
+					} else if (j == h - 1) {
+						tiles [i, j] = new Tile (tileNames [2], tileSize, tileSize, true);
+						placeDoor (ref doorsPlaced, i, j);
+					} else if (i == w - 1) {
+						tiles [i, j] = new Tile (tileNames [3], tileSize, tileSize, true);
+						placeDoor (ref doorsPlaced, i, j);
+					} else if (j == 0) {
+						tiles [i, j] = new Tile (tileNames [4], tileSize, tileSize, true);
+						placeDoor (ref doorsPlaced, i, j);
+					} else {
+						tiles [i, j] = new Tile (tileNames [0], tileSize, tileSize, false);
+					}
+				}
+			}
+		}
+
+		private void placeDoor(ref int doorsPlaced, int i, int j) {
+			Random rng = new Random ();
+			if(rng.Next(0, 5) == 0) {
+				if (doorsPlaced < numberOfDoors) {
+					tiles [i, j] = new Tile (tileNames [0], tileSize, tileSize, false);
+					doorsPlaced++;
+				}
+			}
+		}
+
+		public void renderRoom(int offsetX, int offsetY) {
+			for (int i = 0; i < roomWidth; i++) {
+				for (int j = 0; j < roomHeight; j++) {
+					tiles [i,j].renderTile (Globals.TextureSize, i * Globals.TextureSize * 2 + offsetX, j * Globals.TextureSize * 2 + offsetY);
+				}
+			}
+		}
+
+		public Tile getTileAtLocation(int x, int y) {
+			return tiles [x, y];
+		}
+
+		public int getRoomWidth() {
+			return roomWidth;
+		}
+
+		public int getRoomHeight() {
+			return roomHeight;
+		}
+	}
+}
+
+>>>>>>> origin/dungeon-generation
--- Dungeon-Shambles/Dungeon-Shambles/Main.cs
+++ Dungeon-Shambles/Dungeon-Shambles/Main.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 using System.Drawing;
 using OpenTK;
@@ -174,4 +175,118 @@ namespace DungeonShambles
 
         protected override void OnResize(EventArgs e) { }
     }
+=======
+﻿using System;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace DungeonShambles
+{
+	class Game: GameWindow
+	{
+		// object references to pass between OnLoad and OnRenderFrame
+		MainCharacter mainChar;
+		Dungeon dungeon;
+
+
+		// setup the window width and height
+		public Game() : base(Globals.WindowWidth, Globals.WindowHeight) {}
+
+		public static void Main (string[] args)
+		{
+			using (Game game = new Game())
+			{
+				// run update and game at 30 frames per second
+				game.Run(30, 30);
+			}
+		}
+
+		private void init() {
+			// clear the window to white
+			GL.ClearColor (0.0f, 0.0f, 0.0f, 0.0f);
+
+			GL.MatrixMode (MatrixMode.Modelview);
+
+			GL.LoadIdentity ();
+			// setup the view port
+			GL.Ortho (100, 100, 100, 100, 0, 1);
+			// enable textures to be rendered
+			GL.Enable(EnableCap.Texture2D);
+
+			GL.BlendFunc (BlendingFactorSrc.One, BlendingFactorDest.DstAlpha);
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+			// base setup
+			init ();
+			// set the windows title
+			Title = "Dungeon Shambles";
+			// clear the color of the window to black
+			GL.ClearColor(Color.Black);
+			// create the main character
+			mainChar = new MainCharacter ();
+
+			dungeon = new Dungeon (9);
+
+			dungeon.generateDungeon ();
+		}
+
+		protected override void OnUpdateFrame(FrameEventArgs e)
+		{
+			// variable for checking keyboard input
+			var keyboard = OpenTK.Input.Keyboard.GetState();
+			// left key is pressed
+			if (keyboard [OpenTK.Input.Key.A]) {
+				// increase the main characters x position
+				mainChar.increaseX (-1 * mainChar.getSpeed ());
+				// move the scene around the character in the x position
+				GL.Translate (mainChar.getSpeed (), 0, 0);
+			}
+			// right key is pressed
+			else if (keyboard [OpenTK.Input.Key.D]) {
+				// decrease the main characters x position
+				mainChar.increaseX (mainChar.getSpeed ());
+				// move the scene around the character in the x position
+				GL.Translate (mainChar.getSpeed() * -1, 0, 0);
+			}
+			// up key is pressed
+			if (keyboard [OpenTK.Input.Key.W]) {
+				// increase the main characters y position
+				mainChar.increaseY (mainChar.getSpeed ());
+				// move the scene around the character in the y position
+				GL.Translate (0, mainChar.getSpeed () * -1, 0);
+			}
+			// down key is pressed
+			else if (keyboard [OpenTK.Input.Key.S]) {
+				// decrease the main characters x position
+				mainChar.increaseY (-1 * mainChar.getSpeed ());
+				// move the scene around the character in the y position
+				GL.Translate (0, mainChar.getSpeed (), 0);
+			}
+		}
+
+		protected override void OnRenderFrame(FrameEventArgs e)
+		{
+			// clear the screen
+			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+			dungeon.renderDungeon ();
+
+			GL.Enable (EnableCap.Blend);
+			// render the main character
+			mainChar.renderCharacter ();
+			GL.Disable (EnableCap.Blend);
+
+			// switch between the 2 buffers
+			SwapBuffers();
+		}
+
+		protected override void OnResize(EventArgs e) {}
+	}
+>>>>>>> origin/dungeon-generation
 }
