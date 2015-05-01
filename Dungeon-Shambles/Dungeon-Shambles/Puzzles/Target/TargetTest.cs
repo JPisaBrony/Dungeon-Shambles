1bac73a39955dcba7f33d240a4c1faae9ee2486f
JPisaBrony <jdpohl789@gmail.com>
2015-05-01 18:14:34 -0500

    renamed file

---
+++ Dungeon-Shambles/Dungeon-Shambles/Puzzles/Target/TargetTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace DungeonShambles
+{
+    public class TargetTest
+    {
+        private static ArrayList targets = new ArrayList();
+
+        public static void addTarget(Target target)
+        {
+            targets.Add(target);
+        }
+
+        public static Boolean pressTest(ArrayList rocks)
+        {
+            int count1 = 0;
+            int count2 = 0;
+            foreach (Target target in targets)
+            {
+                count1++;
+                foreach (Rock rock in rocks)
+                {
+                    if((Math.Abs(rock.getX() - target.getX()) < .05 && 
+                        (Math.Abs(rock.getY() - target.getY())) < .05))
+                    {
+                        count2++;
+                    }
+                }
+            }
+            if (count1 == count2)
+                return true;
+            else
+                return false;
+        }
+    }
+}
